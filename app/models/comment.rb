d9d6e45e30208d706db11eec555aa483fe42efb6
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-12-12 14:25:43 +0000

    added A,V, and model relationships

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,4 +10,14 @@
 #  photo_id   :integer
 #
 class Comment < ApplicationRecord
+  validates(:commenter, { :presence => true })
+
+  # Association accessor methods to define:
+  
+  ## Direct associations
+
+  # Comment#commenter: returns a row from the users table associated to this comment by the author_id column
+  belongs_to(:commenter, class_name: "User", foreign_key: "author_id")
+  # Comment#photo: returns a row from the photos table associated to this comment by the photo_id column
+  belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -9,4 +9,18 @@
 #  photo_id   :integer
 #
 class Like < ApplicationRecord
+  validates(:fan, { :presence => true })
+  validates(:photo, { :presence => true })
+  validates(:photo_id, { 
+    :uniqueness => { :scope => [:fan_id] }
+  })
+
+  # Association accessor methods to define:
+  
+  ## Direct associations
+
+  # Like#fan: returns a row from the users table associated to this like by the fan_id column
+  belongs_to(:fan, class_name: "User", foreign_key: "fan_id")
+  # Like#photo: returns a row from the photo table associated to this like by the photo_id column
+  belongs_to(:photo, class_name: "Photo", foreign_key: "photo_id")
 end
