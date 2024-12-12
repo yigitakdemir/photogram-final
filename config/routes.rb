a98ad23f129e2f661da985bc8009769da715227b
yigitakdemir <146138358+yigitakdemir@users.noreply.github.com>
2024-12-12 14:04:47 +0000

    added R,C

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
   root "users#index"
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get("/users", controller: "users", action: "index")
+  get("/users/:username", controller: "users", action: "show")
+  post("/add_user", controller: "users", action: "create")
+  post("/update_user/:user_id", controller: "users", action: "update")
+  get("/photos", controller: "photos", action: "index")
+  get("/photos/:photo_id", controller: "photos", action: "show")
+  get("/delete_photo/:photo_id", controller: "photos", action: "delete")
+  post("/insert_photo_record", controller: "photos", action: "create")
+  post("/update_photo/:photo_id", controller: "photos", action: "update")
+  post("/add_comment", controller: "photos", action: "comment")
+
 end
